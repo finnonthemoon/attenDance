ce7a6fda16ca8bda2cf3f813f5dde34a06dc8074
Finn Elliott <felliot2@sheffield.ac.uk>
2026-03-03 09:42:29 +0000

    Fixed json and javascript, extension now works

--- popup.js
+++ popup.js
@@ -2,8 +2,16 @@ document.getElementById('revealBtn').addEventListener('click', async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
+    target: { tabId: tab.id, allFrames: true },
     func: () => {
+        
+      const frames = document.querySelectorAll('iframe');
+      frames.forEach(frame => {
+        if (frame.src.includes('attendance-student')) {
+          frame.src = frame.src.replace('attendance-student', 'attendance-lecturer');
+        }
+      });
+
       const targets = document.querySelectorAll('.attendance-student');
       targets.forEach(el => {
         el.classList.replace('attendance-student', 'attendance-lecturer');
