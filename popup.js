0d74a2a261fea89b41cc471e356f0c50a60df9cc
Finn Elliott <felliot2@sheffield.ac.uk>
2026-03-09 12:25:25 +0000

    Added an alert with code and fixed json issue

--- popup.js
+++ popup.js
@@ -2,16 +2,30 @@ document.getElementById('revealBtn').addEventListener('click', async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
+    target: { tabId: tab.id, allFrames: true },
     func: () => {
       const attendanceFrame = document.querySelector('iframe[src*="attendance-student"]');
       
       if (attendanceFrame) {
         const newSrc = attendanceFrame.src.replace('attendance-student', 'attendance-lecturer');
         attendanceFrame.src = newSrc;
-        console.log("Successfully redirected iframe to Lecturer View.");
+        console.log("Redirecting to Lecturer View...");
+        
+        return; 
+      }
+
+      const codeElement = document.querySelector('.cm-message strong');
+
+      if (codeElement) {
+          const attendanceCode = codeElement.innerText;
+          alert("Found Code: " + attendanceCode);
+          
+          const inputBox = document.querySelector('input[placeholder*="XX-XX-XX"]');
+          if (inputBox) {
+              inputBox.value = attendanceCode;
+          }
       } else {
-        console.log("Attendance iframe not found on this page.");
+          console.log("Code not found. If the page is still loading, try clicking again in a second.");
       }
     }
   });
